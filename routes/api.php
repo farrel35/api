850aa8caa04030b3362cf95205d197433fd49969
Farrel Ardian <farrelardian123@gmail.com>
2025-03-17 20:52:49 +0700

    add auth register, login, logout

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+});
+
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', fn() => response()->json(['message' => 'Welcome Admin']));
+});
+
+Route::middleware(['auth:sanctum', 'role:owner_bengkel'])->group(function () {
+    Route::get('/owner/dashboard', fn() => response()->json(['message' => 'Welcome Owner']));
+});
+
+Route::middleware(['auth:sanctum', 'role:user'])->group(function () {
+    Route::get('/user/dashboard', fn() => response()->json(['message' => 'Welcome User']));
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
