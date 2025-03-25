71c18f10b78e904c611f72c88c25d5df8db0fb89
Farrel Ardian <farrelardian123@gmail.com>
2025-03-25 21:53:53 +0700

    change update profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -25,10 +25,10 @@ class ProfileController extends Controller
     {
         $user = auth()->user();
 
-        $request->validate([
-            'name' => 'sometimes|string|max:255',
-            'email' => 'sometimes|string|email|unique:users,email,' . $user->id,
-            'password' => 'sometimes|string|min:6',
+        $validated = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|string|email|unique:users,email,' . $user->id,
+            'password' => 'sometimes|required|string|min:6',
             'image' => 'nullable|image|mimes:jpg,png,jpeg|max:2048',
         ]);
 
@@ -36,19 +36,10 @@ class ProfileController extends Controller
             if ($user->image) {
                 Storage::disk('public')->delete($user->image);
             }
-
-            $imagePath = $request->file('image')->store('users', 'public');
-            $user->image = $imagePath;
+            $validated['image'] = $request->file('image')->store('users', 'public');
         }
 
-        if ($request->filled('name'))
-            $user->name = $request->name;
-        if ($request->filled('email'))
-            $user->email = $request->email;
-        if ($request->filled('password'))
-            $user->password = Hash::make($request->password);
-
-        $user->save();
+        $user->update($validated);
 
         return response()->json([
             'message' => 'Profile updated successfully',
