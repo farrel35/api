124d6c85cb5fba335a30fd13519bf357932e9439
Farrel Ardian <farrelardian123@gmail.com>
2025-05-20 02:03:53 +0700

    add user crud filament

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Illuminate\Support\Facades\Hash;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\UserResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+
+class UserResource extends Resource
+{
+    protected static ?string $navigationIcon = 'heroicon-o-user';
+    protected static ?string $navigationLabel = 'Users';
+    protected static ?string $pluralModelLabel = 'Users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')->required()->maxLength(255),
+                TextInput::make('email')->required()->email()->unique(ignoreRecord: true),
+                TextInput::make('password')
+                    ->password()
+                    ->dehydrateStateUsing(fn($state) => filled($state) ? Hash::make($state) : null)
+                    ->required(fn(string $context) => $context === 'create')
+                    ->label('Password')
+                    ->maxLength(255),
+                TextInput::make('no_hp')->label('Phone Number')->required(),
+                Select::make('role')
+                    ->options([
+                        'user' => 'User',
+                        'admin_bengkel' => 'Admin Bengkel',
+                        'admin' => 'Admin',
+                    ])
+                    ->required(),
+                FileUpload::make('image')->image()->directory('user-images'),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
+                TextColumn::make('no_hp')->label('Phone'),
+                TextColumn::make('role')->sortable(),
+            ])
+            ->filters([])
+            ->actions([
+                Tables\Actions\EditAction::make()
+                    ->visible(fn(User $record) => $record->role !== 'admin'),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => UserResource\Pages\ListUsers::route('/'),
+            'create' => UserResource\Pages\CreateUser::route('/create'),
+            'edit' => UserResource\Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
